e8bc9f9d6d29edf620310c651336f03e9b9ed508
CJason14 <constantinjasonkautz@gmail.com>
2022-11-03 18:37:10 +0100

    Update

--- Job/static/Job/jobs.js
+++ Job/static/Job/jobs.js
@@ -17,9 +17,24 @@ function loadjobs() {
                 const title = document.createElement("h3");
                 const title_content = document.createTextNode(jobs[count].title);
                 title.appendChild(title_content);
-                job_div.appendChild(title)
+                job_div.appendChild(title);
+
+                const salary = document.createElement("h6");
+                const salary_content = document.createTextNode("Salary: " + jobs[count].salary + "$");
+                salary.appendChild(salary_content);
+                job_div.appendChild(salary);
+
+                const category = document.createElement("h6");
+                const category_content = document.createTextNode("Category: " + jobs[count].category);
+                category.appendChild(category_content);
+                job_div.appendChild(category);
 
                 job_div.appendChild(document.createElement("hr"));
+
+                const description = document.createElement("p");
+                const description_content = document.createTextNode(jobs[count].description);
+                description.appendChild(description_content);
+                job_div.appendChild(description);
 
                 document.getElementById('Jobs').appendChild(job_div);
                 count += 1
